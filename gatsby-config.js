1797fbd30c5413d1142e8740a5eb63c2c2705d21
Kamil Jozwik <kamiljozwikpl@gmail.com>
2019-01-22 23:30:06 +0100

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     'gatsby-transformer-remark',
+    'gatsby-plugin-favicon',
     {
       resolve: `gatsby-source-contentful`,
       options: {
