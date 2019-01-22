4b7f91f1753c91ca40dffcefa8a30d2b8e35c4de
Kamil Jozwik <kamiljozwikpl@gmail.com>
2019-01-22 23:13:53 +0100

    wyl SW po problemach na mobile

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,38 +37,18 @@ module.exports = {
         cookieDomain: 'www.frontstack.pl',
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'front-stack',
-        short_name: 'frontstack',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/styles/img/favicon.png', // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-129884825-1',
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Enables Google Optimize using your container Id
-        // optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'frontstack.pl',
-      },
-    },
-    'gatsby-plugin-offline',
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: 'front-stack',
+    //     short_name: 'frontstack',
+    //     start_url: '/',
+    //     background_color: '#663399',
+    //     theme_color: '#663399',
+    //     display: 'minimal-ui',
+    //     icon: 'src/styles/img/favicon.png', // This path is relative to the root of the site.
+    //   },
+    // },
+    // 'gatsby-plugin-offline',
   ],
 };
