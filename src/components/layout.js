29df6742f1525cc182287adfd03e2c1e482b62a6
kamil.jozwik <kamil.jozwik@erecruiter.pl>
2019-03-05 16:46:43 +0100

    hooks1

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import browser from 'browser-detect';
 import { CSSPlugin, AttrPlugin } from 'gsap/all';
@@ -12,45 +12,43 @@ import '../styles/index.scss';
 
 // without this line, CSSPlugin and AttrPlugin may get dropped by your bundler...
 const plugins = [CSSPlugin, AttrPlugin]; // eslint-disable-line
-class Layout extends Component {
-  constructor(props) {
-    super(props);
-    this.sidebarMenuWrapper = React.createRef();
-  }
 
-  render() {
+function Layout({ type, children }) {
+  const sidebarMenuWrapperRef = useRef(null);
+
+  useEffect(() => {
     const detectedBrowser = browser();
     if (!detectedBrowser.name.match(/^(chrome|firefox|safari|opera|edge|ios)$/) && typeof window !== 'undefined') {
       browserPopup(detectedBrowser.name);
     }
-    return (
-      <StaticQuery
-        query={graphql`
-          query SiteTitleQuery {
-            site {
-              siteMetadata {
-                title
-              }
-            }
-          }
-        `}
-        render={dataQuery => (
-          <HeaderProvider>
-            <Topbar />
-            <div className="sidebar" ref={this.sidebarMenuWrapper}>
-              <Sidebar />
-            </div>
-            <div className={this.props.type}>
-              { this.props.children }
-            </div>
-            <Footer />
-          </HeaderProvider>
-        )}
-      />
-    );
-  }
-}
+  }, []);
 
+  return (
+    <StaticQuery
+      query={graphql`
+              query SiteTitleQuery {
+                site {
+                  siteMetadata {
+                    title
+                  }
+                }
+              }
+            `}
+      render={dataQuery => (
+        <HeaderProvider>
+          <Topbar />
+          <div className="sidebar" ref={sidebarMenuWrapperRef}>
+            <Sidebar />
+          </div>
+          <div className={type}>
+            { children }
+          </div>
+          <Footer />
+        </HeaderProvider>
+      )}
+    />
+  );
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,78 +1,61 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import debounce from 'lodash/debounce';
+import React, { useContext, useRef, useState, useEffect } from 'react';
+// import debounce from 'lodash/debounce';
 import { slide as Menu } from 'react-burger-menu';
 import { Link } from 'gatsby';
 import { HeaderContext } from '../../HeaderContext';
 
-class Sidebar extends Component {
-  static contextType = HeaderContext;
+function Sidebar() {
+  const context = useContext(HeaderContext);
+  const sidebarMenu = useRef(null);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(0);
 
-  constructor(props) {
-    super(props);
-    this.sidebarMenu = React.createRef();
-    this.state = {
-      menuOpen: false,
-      windowWidth: 0
-    };
-  }
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    // window.addEventListener('resize', debounce(setWindowWidth(window.innerWidth), 300));
+    window.addEventListener('resize', setWindowWidth(window.innerWidth));
+  }, []);
 
-  componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener('resize', debounce(this.updateWindowDimensions, 300));
-  }
-
-  updateWindowDimensions = () => {
-    this.setState(prevState => ({
-      ...prevState,
-      windowWidth: window.innerWidth
-    }));
-  }
-
-  switchMenu = (event) => {
+  function switchMenu(event) {
     event.preventDefault();
     event.target.classList.toggle('open');
-    this.setState(prevState => ({
-      menuOpen: !prevState.menuOpen
-    }));
+    setMenuOpen(prevMenuOpen => !prevMenuOpen);
   }
 
-  isTabActive = link => this.context.currentCategory === link ? 'active' : ''; // eslint-disable-line
+  const isTabActive = (link) => context.currentCategory === link ? 'active' : ''; // eslint-disable-line
 
-  render() {
-    return (
-      <Menu
-        ref={this.sidebarMenu}
-        isOpen={this.state.windowWidth > 600 ? true : this.state.menuOpen}
-        width={70}
-        noOverlay
-        className="sidebar__menu"
-        customBurgerIcon={false}
-        customCrossIcon={false}
-        menuClassName="sidebar__wrapper"
-        itemListClassName="sidebar__items"
+  return (
+    <Menu
+      ref={sidebarMenu}
+      isOpen={windowWidth > 600 ? true : menuOpen}
+      width={70}
+      noOverlay
+      className="sidebar__menu"
+      customBurgerIcon={false}
+      customCrossIcon={false}
+      menuClassName="sidebar__wrapper"
+      itemListClassName="sidebar__items"
+    >
+      <a
+        data-link="Open"
+        className="sidebar__burger"
+        onClick={switchMenu}
+        onKeyPress={switchMenu}
+        role="button"
+        tabIndex="0"
       >
-        <a
-          data-link="Open"
-          className="sidebar__burger"
-          onClick={this.switchMenu}
-          onKeyPress={this.switchMenu}
-          role="button"
-          tabIndex="0"
-        >
-          {''}
-        </a>
-        <Link className={`${this.isTabActive('')}`} data-link="Main" to="/" />
-        <Link className={`${this.isTabActive('show')}`} data-link="Show" to="/show/">Show</Link>
-        <Link className={`${this.isTabActive('js')}`} data-link="JS" to="/js/">JavaScript</Link>
-        <Link className={`${this.isTabActive('web')}`} data-link="Web" to="/web/">WEB</Link>
-        <Link className={`${this.isTabActive('voice')}`} data-link="Voice" to="/voice/">Głos</Link>
-        <Link className={`${this.isTabActive('frontops')}`} data-link="FrontOps" to="/frontops/">FrontOps</Link>
-        <Link className={`${this.isTabActive('api')}`} data-link="API" to="/api/">API</Link>
-        <Link className={`${this.isTabActive('prod')}`} data-link="Prod" to="/prod/">Prod.</Link>
-      </Menu>
-    );
-  }
+        {''}
+      </a>
+      <Link className={`${isTabActive('')}`} data-link="Main" to="/" />
+      <Link className={`${isTabActive('show')}`} data-link="Show" to="/show/">Show</Link>
+      <Link className={`${isTabActive('js')}`} data-link="JS" to="/js/">JavaScript</Link>
+      <Link className={`${isTabActive('web')}`} data-link="Web" to="/web/">WEB</Link>
+      <Link className={`${isTabActive('voice')}`} data-link="Voice" to="/voice/">Głos</Link>
+      <Link className={`${isTabActive('frontops')}`} data-link="FrontOps" to="/frontops/">FrontOps</Link>
+      <Link className={`${isTabActive('api')}`} data-link="API" to="/api/">API</Link>
+      <Link className={`${isTabActive('prod')}`} data-link="Prod" to="/prod/">Prod.</Link>
+    </Menu>
+  );
 }
 
 export default Sidebar;
